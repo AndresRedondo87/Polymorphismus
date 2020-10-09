38d595798b62d2eddd08e7dd1f697548af1847fc
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-10-09 12:27:05 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphismus
+{
+    class Program
+    {
+
+        // Erstelle eine Basisklasse Auto mit zwei Eigenschaften "PS" und "Farbe"
+        // Erstelle einen Konstruktor, der diese beiden Eigenschaften setzt.
+        // Erstelle eine Methode namens ZeigeDetails(), die die PS und die Farbe des Fahrzeugs auf der Konsole anzeigt.
+        // Erstelle eine Reparaturmethode, die "Auto wurde repariert" auf die Konsole schreibt. 
+        // Erstellen Sie zwei abgeleitete Klassen, BMW und Audi, die über einen eigenen Konstruktor verfügen 
+        // und eine zusätzliche Eigenschaft, Model genannt, haben.
+        // Auch ein privates Mitglied namens Marke. Marke sollte in jeder der beiden Klassen unterschiedlich sein.
+        // Erstelle die beiden Methoden ZeigeDetails() und Repair auch in diesen beiden Klassen.Passe diese Methoden entsprechend an.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polymorphismus...");
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+
+        }
+    }
+}
